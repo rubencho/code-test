cc480395f900c9c60525e40511c1105471b6cb1c
rubencho <ruben.rhidalgo@gmail.com>
2021-02-26 15:47:56 +1100

    Improve the code

--- bowling.py
+++ bowling.py
@@ -1,8 +1,14 @@
-
 from dataclasses import dataclass
+
 
 PINS_IN_A_FRAME = 10
 FRAMES_IN_A_MATCH = 10
+
+class More10FramesInThisGame(Exception):
+    pass
+
+class GameOver(Exception):
+    pass
 
 @dataclass
 class Frame:
@@ -15,66 +21,86 @@ class Frame:
     bonus_rolls: int = 0
 
     def sum_pins(self):
-        total_pins = self.first + self.second + self.third
-        if not isinstance(total_pins, int) and 0 < total_pins > 10 :
-            raise ValueError(f'There is something wrong with pins on frame {self}.') 
-        self.frame_score = total_pins
+        return self.first + self.second + self.third
+    
+    def make_score(self):
+        self.frame_score = self.sum_pins()
 
     def is_strike(self) -> bool:
         return bool(self.first == PINS_IN_A_FRAME)
-    
+
     def is_spare(self) -> bool:
         return bool((self.first < PINS_IN_A_FRAME) and (self.frame_score == PINS_IN_A_FRAME))
 
 
-class More10FramesInThisGame(Exception):
-    pass
-
 class BowlingGame:
     """ Give the score for a single bowling player
     """
+
     def __init__(self):
         self._match_frames = list()
-        self._first_roll = True
+        self._rolls_frame = 0
+        self._game_over = False
 
     def score(self) -> int:
         return sum(f.frame_score for f in self._match_frames)
 
     def roll(self, pins: int):
+        self._fill_frames_rolls(pins)
+        self._update_frame_score(pins)
+
+    #========== Fill rolls on frames ===========
+    def _fill_frames_rolls(self, pins: int):
         self._check_if_frames_is_valid()
         self._check_if_invalid_roll(pins)
-        self._fill_frames_rolls(pins)
-        self._update_frame_score()
-        self._update_strike_or_spare()
-        self._update_frame_bonus(pins)
 
-    def _fill_frames_rolls(self, pins: int):
-        if len(self._match_frames) >= 9 and pins == 10:
-            if self._first_roll == True:
-                self._match_frames.append(Frame(first=pins))
-                self._first_roll = False
-            if self._first_roll == False:
-                self._match_frames[-1].second = pins
-                self._first_roll = 3
-            if self._first_roll == 3:
-                self._match_frames[-1].third = pins
+        if self._frame_10th():
+            self._fill_frame_10th(pins)
 
-        elif pins == PINS_IN_A_FRAME and self._first_roll == True: # Case of strike
-            self._match_frames.append(Frame(first=pins))
+        elif self._rolls_frame == 0:
+            self._match_frames[-1].first = pins
+            if  pins != PINS_IN_A_FRAME:  # Case IS NOT a strike
+                self._rolls_frame = 1
 
-        elif self._first_roll == True:
-            self._match_frames.append(Frame(first=pins))
-            self._first_roll = False
-
-        elif self._first_roll == False:
+        elif self._rolls_frame == 1:
             self._check_sum_of_pins_in_last_frame(pins)
             self._match_frames[-1].second = pins
-            self._first_roll = True
+            self._rolls_frame = 0
 
-    def _update_frame_score(self):
-        self._match_frames[-1].sum_pins()
+    def _fill_frame_10th(self, pins: int):
+        if self._rolls_frame == 0:
+            self._match_frames[-1].first = pins
+            self._rolls_frame = 1
 
-    def _update_strike_or_spare(self):
+        elif self._rolls_frame == 1:
+            self._match_frames[-1].second = pins
+            self._match_frames[-1].make_score()
+            if self._match_frames[-1].is_strike() or self._match_frames[-1].is_spare() or pins == PINS_IN_A_FRAME:
+                self._rolls_frame = 2
+            else:
+                self._game_over = True
+
+        elif self._rolls_frame == 2:
+            self._match_frames[-1].third = pins
+            self._game_over = True
+
+    #============ Score code ================
+
+    def _update_frame_score(self, pins: int):
+        self._make_frame_score()
+        self._update_strike_or_spare_extra_rolls()
+        self._update_frame_bonus(pins)
+
+    def _make_frame_score(self):
+        self._match_frames[-1].make_score()
+        if not self._frame_10th() and self._match_frames[-1].frame_score > 10:
+            raise Exception(
+                f'The sum of pins is incorret! Check if total score is between 0 and 10.')
+
+    def _update_strike_or_spare_extra_rolls(self):
+        """ Give the extra rolls to get the bonus point -2 or 1-
+            in case strike or spare, respectivelly.
+        """
         cf = self._match_frames[-1]
         if cf.is_strike():
             rolls_extra = 2
@@ -88,22 +114,38 @@ class BowlingGame:
         """ Check if last two frames need a bonus
             due to frame is strike or spare
         """
+
         for frame in self._match_frames[-3:-1]:
             if frame.bonus_rolls > 0:
                 frame.frame_score += pins
                 frame.bonus_rolls -= 1
-
-    def _check_if_frames_is_valid(self):
-        if len(self._match_frames) > 10:
-            raise More10FramesInThisGame('There is more than {FRAMES_IN_A_MATCH} in this game.')
     
+    #======== Validation functions =========
+
+    def _total_pins(self):
+        return self._match_frames[-1].sum_pins()
+    
+    def _frame_10th(self):
+        return len(self._match_frames) == FRAMES_IN_A_MATCH
+    
+    def _check_if_invalid_roll(self, pins: int):
+        if not 0 <= pins <= 10 or not isinstance(pins, int):
+            raise Exception(
+                f'The value of {pins} pins is incorret! Check if rolls is between 0 and {PINS_IN_A_FRAME}.')
+
     def _check_sum_of_pins_in_last_frame(self, pins: int):
         if self._match_frames[-1].first + pins > PINS_IN_A_FRAME:
-            raise ValueError('Something wrong with the sum of pins in this frame.')
+            raise ValueError(
+                f'Something wrong with the sum of pins in this frame. Check if is frame score is between 0 and {PINS_IN_A_FRAME}.')
 
-    @staticmethod
-    def _check_if_invalid_roll(pins: int):
-        if pins > 10 or pins < 0:
-            raise Exception(f" The value of {pins} pins is incorret! Check if is between 0 and 10")
+    def _check_if_frames_is_valid(self):
+        if self._game_over:
+            raise GameOver('The game is finish')
+
+        if self._rolls_frame == 0:  # Check if a new frame is needed
+            if self._frame_10th():
+                raise More10FramesInThisGame(
+                    f'There is more than {FRAMES_IN_A_MATCH} frames in this game.')            
+            self._match_frames.append(Frame())
 
 
--- test.py
+++ test.py
@@ -1,19 +1,20 @@
 import unittest
 
-from bowling import BowlingGame, More10FramesInThisGame
+from bowling import BowlingGame, More10FramesInThisGame, GameOver
+
 
 class TestBowling(unittest.TestCase):
 
     def setUp(self):
         self.g = BowlingGame()
-    
+
     def roll_many(self, n, pins):
         for _ in range(n):
             self.g.roll(pins)
 
     def roll_spare(self):
-        self.g.roll(5)
-        self.g.roll(5)
+        self.g.roll(4)
+        self.g.roll(6)
 
     def roll_strike(self):
         self.g.roll(10)
@@ -22,12 +23,32 @@ class TestBowling(unittest.TestCase):
         with self.assertRaises(Exception):
             self.g.roll(11)
 
-
     def test_error_pins_lower_than_0(self):
         with self.assertRaises(Exception):
             self.g.roll(-1)
 
-    def test_gutter_game(self):
+    def test_error_pins_float_number(self):
+        with self.assertRaises(Exception):
+            self.g.roll(2.3)
+
+    def test_error_sum_rolls_greater_than_10_then_correct_score(self):
+        self.g.roll(6)
+        with self.assertRaises(Exception):
+            self.g.roll(5)
+        self.g.roll(3)
+        self.assertEqual(self.g.score(), 9)
+
+    def test_error_more_than_21_rolls_raises_error(self):
+        self.roll_many(20, 0)
+        with self.assertRaises(GameOver):
+            self.g.roll(3)
+
+    def test_first_two_4_pins_rolls_and_get_score(self):
+        self.g.roll(4)
+        self.g.roll(4)
+        self.assertEqual(self.g.score(), 8)
+
+    def test_all_ceros(self):
         self.roll_many(20, 0)
         self.assertEqual(self.g.score(), 0)
 
@@ -37,27 +58,30 @@ class TestBowling(unittest.TestCase):
 
     def test_one_spare(self):
         self.roll_spare()
-        self.g.roll(3)
-        self.roll_many(17, 0)
-        self.assertEqual(self.g.score(), 16)
+        self.g.roll(5)
+        self.g.roll(0)
+        self.assertEqual(self.g.score(), 20)
 
     def test_one_strike(self):
         self.roll_strike()
-        self.g.roll(3)
+        self.g.roll(5)
         self.g.roll(4)
-        self.roll_many(16, 0)
-        self.assertEqual(self.g.score(), 24)
+        self.assertEqual(self.g.score(), 28)
+
+    def test_spare_on_frame_10(self):
+        self.roll_many(18, 0)
+        self.g.roll(5)
+        self.g.roll(5) 
+        self.g.roll(3)
+        self.assertEqual(self.g.score(), 13)
 
     def test_perfect_game(self):
-        self.roll_many(12, 10)
+        self.roll_many(9, 10)
+        self.g.roll(10)
+        self.g.roll(10)
+        self.g.roll(10)
         self.assertEqual(self.g.score(), 300)
 
-    def test_more_than_21_rolls_raises_error(self):
-        self.roll_many(18, 0)
-        self.roll_spare()
-        self.g.roll(0)
-        with self.assertRaises(More10FramesInThisGame):
-            self.g.roll(3)
 
 if __name__ == '__main__':
     unittest.main()
